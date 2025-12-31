a304ba349f9a03286e80b7fcd7ee3a715b577fb9
wenwenba2020 <wenwenba2020@outlook.com>
2025-12-31 01:14:30 -0800

    fix: 修复资源路径问题 - 添加basePath配置

---
+++ _next/static/JJtG6UphYh7QPcWMWy6dw/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/smart-compliance-system//_next/:path+",
+        "destination": "/smart-compliance-system/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
